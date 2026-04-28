d1fb2dd71dc4cebff14776eca8ff298c7e86fd17
janyacollege22-glitch <janyacollege22@gmail.com>
2026-04-28 16:34:57 -0400

    Delete Recitation5.py

--- Recitation5.py
+++
@@ -1,9 +0,0 @@
-def intro_message(count):
-    if count > 0:
-        print("Iam a recursive function")
-        intro_message(count-1)
-    else:
-        print('End of recursion')
-def intro_message_iter():
-    print('Iam a recursive function')
-    intro_message_iter()
